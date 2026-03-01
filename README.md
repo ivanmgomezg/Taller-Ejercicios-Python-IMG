3f340ec3b2f89a778bd198bdd6ac765ce541ee1c
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-03-01 00:44:52 +0000

    feat: agregar solución ejercicio 05

--- README.md
+++ README.md
@@ -127,7 +127,7 @@ A continuación se listan los 30 ejercicios. **Debe escribir el valor exacto de 
 | 02 | ¿Cuántas veces aparece el nombre "Maria" en el dataset? | `4.160` |
 | 03 | ¿Cuántas veces aparece el nombre "Juan" en el dataset? | `3.986` |
 | 04 | ¿Cuál es el nombre más frecuente y cuántas veces aparece? | `Gonzalo - 4.221` |
-| 05 | ¿Cuál es el apellido más frecuente y cuántas veces aparece? | `PENDIENTE` |
+| 05 | ¿Cuál es el apellido más frecuente y cuántas veces aparece? | `Reyes - 7.490` |
 | 06 | ¿Cuántos registros tienen la ciudad "Bogota" después de limpiar? | `PENDIENTE` |
 | 07 | ¿Cuántos registros tienen la ciudad "Medellin" después de limpiar? | `PENDIENTE` |
 | 08 | ¿Cuántas ciudades únicas existen después de normalizar? | `PENDIENTE` |
---
+++ soluciones/05.py
@@ -0,0 +1,34 @@
+# ==================================================================================
+# Ejercicio 05
+# Descripción: ¿Cuál es el apellido más frecuente y cuántas veces aparece?
+# ==================================================================================
+
+# Importamos las librerias necesarias
+import sys
+import os
+
+# Le decimos a Python dónde está la raíz del proyecto
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Importamos el módulo de conexión y otros desde la carpeta utils para cargar los datos
+import utils.conexion as conexion
+import utils.transformaciones as transf
+
+# Cargamos el CSV en un DataFrame para poder manipularlo
+df = conexion.cargar_datos_csv("personas.csv") 
+
+# -------------------------------------------------------
+
+df["apellido_cifrado"] = df["apellido_cifrado"].apply(lambda x: transf.rot13(x)) #Decodificamos la columna "apellido_cifrado" usando ROT13 para obtener los nombres reales 
+
+
+# Contamos la frecuencia de cada nombre en la columna "apellido_cifrado"
+conteo = df["apellido_cifrado"].value_counts() 
+
+# Obtenemos el nombre más frecuente utilizando idxmax() que devuelve el índice del valor máximo en la serie de conteo
+nombre_mas_frecuente = conteo.idxmax() 
+
+# Obtenemos la frecuencia del nombre más frecuente utilizando max() que devuelve el valor máximo en la serie de conteo
+frecuencia = conteo.max() 
+
+print(f"El apellido más frecuente es '{nombre_mas_frecuente}' y aparece {frecuencia} veces en el dataset") 
