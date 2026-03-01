2eed52a7f714a34b1edbcf205e28b192206dd05b
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-03-01 00:42:11 +0000

    feat: agregar solución ejercicio 02

---
+++ soluciones/02.py
@@ -0,0 +1,31 @@
+# ==================================================================================
+# Ejercicio 02
+# Descripción: ¿Cuántas veces aparece el nombre "Maria" en el dataset?
+# ==================================================================================
+
+# manejo de rutas y configuración del entorno de ejecución
+import sys
+import os
+
+# Le decimos a Python dónde está la raíz del proyecto
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Importamos el módulo de conexión y otros desde la carpeta utils para cargar los datos
+import utils.conexion as conexion
+import utils.transformaciones as transf
+
+# Cargamos el CSV en un DataFrame para poder manipularlo
+df = conexion.cargar_datos_csv("personas.csv") 
+
+# -------------------------------------------------------
+
+#Decodificamos la columna "nombre_cifrado" usando ROT13 para obtener los nombres reales 
+df["nombre_cifrado"] = df["nombre_cifrado"].apply(lambda x: transf.rot13(x)) 
+
+# Nombre que queremos buscar en el dataset
+nom_buscar = "Maria" 
+
+# Contamos cuantas veces aparece "Maria" en la columna "nombre_cifrado"
+contar = (df["nombre_cifrado"]==nom_buscar).sum() 
+
+print(f"El nombre '{nom_buscar}' aparece {contar} veces en el dataset")
