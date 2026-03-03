7197deb0dde6e36cb6148f98265ad8a7d2185caf
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-03-03 17:30:51 +0000

    feat: agregar solución ejercicio 22

--- README.md
+++ README.md
@@ -177,7 +177,7 @@ A continuación se listan los 30 ejercicios. **Debe escribir el valor exacto de 
 | 19 | ¿Cuántos registros tienen fecha de nacimiento con formato diferente a YYYY-MM-DD? | `89.823` |
 | 20 | ¿Cuántas personas nacieron entre 1990 y 2000 (inclusive)? | `53.404` |
 | 21 | ¿Cuántas personas nacieron antes de 1960? | `66.577` |
-| 22 | ¿Cuántas personas tienen más de 50 años (fecha actual: 2026-02-26)? | `PENDIENTE` |
+| 22 | ¿Cuántas personas tienen más de 50 años (fecha actual: 2026-02-26)? | `140.134` |
 | 23 | ¿Cuántos registros tienen nombre "Carlos" y viven en "Cali"? | `PENDIENTE` |
 | 24 | ¿Cuántos registros tienen nombre "Ana" y son "Medico"? | `PENDIENTE` |
 | 25 | ¿Cuántos registros tienen profesión "Abogado" y salario > 10,000,000? | `PENDIENTE` |
---
+++ soluciones/22.py
@@ -0,0 +1,29 @@
+# ==================================================================================
+# Ejercicio 21
+# ¿Cuántas personas tienen más de 50 años (fecha actual: 2026-02-26)?
+# ==================================================================================
+
+# Importamos las librerias necesarias
+import sys
+import os
+from datetime import datetime
+
+# Le decimos a Python dónde está la raíz del proyecto
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import utils.limpieza as limp
+import utils.conexion as conexion
+
+#Conexion y limpieza de datos
+df = conexion.cargar_datos_csv("personas.csv")
+df_clean = limp.limpiar_dataset(df)
+
+# Calculamos la edad de cada persona
+fecha_actual = datetime(2026, 2, 26)
+
+#nueva columna edad con el calculo de la edad a partir de la fecha de nacimiento
+df_clean['edad'] = (fecha_actual - df_clean['fecha_nacimiento']).dt.days // 365
+
+# Contamos cuántas personas tienen más de 50 años
+personas_mayores_50 = df_clean[df_clean['edad'] > 50].shape[0]
+print(f"El número de personas con más de 50 años es: {personas_mayores_50}")    
